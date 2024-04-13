47badb67a26537e6a4ab575138c90bf7e23dc6b4
Marko Markovic <markomarkoviccb@gmail.com>
2024-04-13 13:40:56 +0200

    135. Assigment

--- src/main/java/guru/springframework/model/Ingredient.java
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -11,7 +11,8 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
@@ -37,6 +38,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
     public Recipe getRecipe() {
